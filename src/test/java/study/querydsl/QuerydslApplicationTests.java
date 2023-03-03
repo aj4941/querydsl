0da15b4c595a58aa1c68ee5afd1b9423f229db98
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-03 23:51:33 +0900

    JPQL, Querydsl 추가

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import study.querydsl.entity.Hello;
-import study.querydsl.entity.QHello;
 
 import javax.persistence.EntityManager;
 
@@ -20,19 +18,5 @@ class QuerydslApplicationTests {
 	@Autowired EntityManager em;
 
 	@Test
-	void contextLoads() {
-		Hello hello = new Hello();
-		em.persist(hello);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello; // new QHello("h");
-
-		Hello result = query
-				.selectFrom(qHello)
-				.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
-	}
-
+	void contextLoads() {}
 }
