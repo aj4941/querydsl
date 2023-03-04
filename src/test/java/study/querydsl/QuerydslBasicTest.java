2ce14f3bf36f1c6c060ea34ea26323180f715bea
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-04 21:51:37 +0900

    조인 관련 테스트

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -167,7 +167,7 @@ public class QuerydslBasicTest {
                         member.age.sum(),
                         member.age.avg(),
                         member.age.max(),
-                        member.age.min())
+                        member.age.min()) // Member 객체가 아닌 여러 값을 조회할 때 Tuple 반환
                 .from(member)
                 .fetch();
 
@@ -183,7 +183,7 @@ public class QuerydslBasicTest {
         List<Tuple> result = queryFactory
                 .select(team.name, member.age.avg())
                 .from(member)
-                .join(member.team, team)
+                .join(member.team, team) // 조인
                 .groupBy(team.name)
                 .fetch();
 
@@ -195,5 +195,78 @@ public class QuerydslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)// 기본 inner join (on 조건이 없으면 id 값으로 조인)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        // 객체에서 특정 필드를 골라서 값의 유무를 확인할 때 사용 (정확하게 값이 같은지 확인 - containsExactly)
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    // 연관관계가 없어도 조인할 수 있음
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team) // 세타 조인 -> left, right join 불가능
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
+    // select m, t from Member m left join m.team t on t.name = 'teamA'
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team) // 여러 값을 조회하므로 Tuple 반환
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA")) // id가 같아야 하는 조건에 name이 "teamA" 인 조건이 추가된 것
+                .fetch();                       // id 조건이 사라지지 않았음에 유의 (기본 조건)
+
+        // 내부 조인의 경우 on 절이 where절과 일치하다는 것을 알 수 있음
+        // -> 내부 조인은 where을 사용하고 외부 조인은 on 절을 사용
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    // 연관관계가 없는 엔티티 외부 조인
+    // -> 회원의 이름이 팀 이름과 같은 대상을 외부 조인
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team) // leftJoin(member.team, team)과 다름 (연관관계 없이 세타 조인)
+                .on(member.username.eq(team.name)) // 조인 조건 추가 (id 값은 조인 조건이 아니고 name만 설정됨)
+                .fetch();
+
+        // leftJoin(member.team, team)의 경우에는 id 값이 들어가므로 id 값이 기본 조인 조건이 됨
+        // leftJoin(team)은 id가 기본 조인 조건이 되지 않음
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 }
